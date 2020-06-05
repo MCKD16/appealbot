d1e21b41d0e2a6b2d9ac1786705f7fcb8d3915ce
MCKD16 <66161038+MCKD16@users.noreply.github.com>
2020-06-05 21:41:57 +0900

    Update py.py

--- py.py
+++ py.py
@@ -29,7 +29,7 @@ async def on_message(message):
         await client.send_message(message.channel, "Commands:\n```!help - Appeal Bot에 대한 명령어들을 확인합니다.\n!appeal (playerName) - 당신의 벤 또는 뮤트 항소를 합니다.\n!accept (discordID) - 해당 유저가 항소 한 것을 수락합니다. [관리자만]\n!deny (discordID) - 해당 유저가 항소 한 것을 거절합니다.```")
       if message.content[1:7] == "appeal":
         if message.content[8:]:
-          await client.send_message(discord.utils.get(client.get_all_channels(), id="718434205448536066"), "@everyone\n[ " + message.author.mention + "님의 항소 정보 ]```디스코드 이름: " + message.author.mention + "\n디스코드 아이디: " + message.author.id + "\n마인크래프트 닉네임: " + message.content[8:])
+          await client.send_message(discord.utils.get(client.get_all_channels(), id="718434205448536066"), "@everyone\n[ " + message.author.mention + "님의 항소 정보 ]```디스코드 이름: " + message.author.mention + "\n디스코드 아이디: " + message.author.id + "\n마인크래프트 닉네임: " + message.content[8:] + "```")
         else:
           await client.send_message(message.channel, "```마인크래프트 닉네임을 작성하여 주세요.```")
   if message.content[1:7] == "accept":
