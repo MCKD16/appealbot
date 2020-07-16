1b4dfae79909f0c37368e9a00dd3dc242f350379
MCKD16 <66161038+MCKD16@users.noreply.github.com>
2020-07-16 22:18:58 +0900

    Update py.py

--- py.py
+++ py.py
@@ -1,41 +1,29 @@
 import discord
 import os
-import datetime
-
 
 client = discord.Client()
-
 
 @client.event
 async def on_ready():
   print("login")
-  print(client.user.name)
-  print(client.user.id)
   print("------------------")
-  await client.change_presence(game=discord.Game(name='appealbot.xyz', type=1))
-
-@client.event
-async def on_member_join(member):
-  channel = discord.utils.get(member.guild.channels, name="welcome")
-  await channel.send(f"[member.mention]님 환영합니다!")
+  await client.change_presence(game=discord.Game(name='안녕하세요.', type=1))
 
 @client.event
 async def on_message(message):
-  if message.content == "!":
-    if message.channel.is_private and message.author.id != "718421206059057234":
-      await client.send_message(message.channel, "```올바르지 않는 명령어 형식입니다.```")
-  else:
-    if message.content[1:5] == "help":
-      if message.channel.is_private and message.author.id != "718421206059057234":
-        await client.send_message(message.channel, "Commands:\n```!help - Appeal Bot에 대한 명령어들을 확인합니다.\n!appeal (playerName) - 당신의 벤 또는 뮤트 항소를 합니다.```")
-    if message.content[1:7] == "appeal":
-      if message.channel.is_private and message.author.id != "718421206059057234":
-        if message.content[8:]:
-          await client.send_message(discord.utils.get(client.get_all_channels(), id="718434205448536066"), "@everyone\n[ " + message.author.mention + "님의 항소 정보 ]\n디스코드 이름: " + message.author.mention + "\n디스코드 아이디: " + message.author.id + "\n마인크래프트 닉네임: " + message.content[8:])
-          await client.send_message(message.channel, "```성공적으로 항소하셨습니다!```")
+  if message.channel.is_private and message.author.id != "733310767973138553":
+    if message.content == "!":
+      await client.send_message(message.channel, "```정확하지 않은 명령어입니다.```")
+    else:
+      if message.content[1:4] == "말하기":
+        if message.author.id == "623502843558756394":
+          if message.content[8:]:
+            await client.send_message(discord.utils.get(client.get_all_channels(), id="713704015384543262"), "@everyone\n```" + message.content[8:] + "```")
+          else:
+            await client.send_message(message.channel, "```보낼 메세지를 적어주세요.```")
         else:
-          await client.send_message(message.channel, "```마인크래프트 닉네임을 작성하여 주세요.```")
+          await client.send_message(message.channel, "```당신은 이 명령어를 사용할 권한이 없습니다.```")
 
 
-access_token = os.environ["BOT_TOKEN"]
+access_token = os.environ["TOKEN"]
 client.run(access_token)
